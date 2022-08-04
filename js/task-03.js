4adbe55ce432f38bd7095ff01b3607ce7cd524b5
strangedk <strangedik@gmail.com>
2022-08-04 22:40:01 +0100

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryEl = document.querySelector('.gallery');
+
+const imgsHTML = images.map(({url,alt}) => `
+    <li><img src="${url}" alt="${alt}"></li>
+`).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', imgsHTML);
