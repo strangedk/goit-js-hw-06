43991784285e004eff069efdba190fb41937f5ac
strangedk <strangedik@gmail.com>
2022-08-04 22:41:27 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, 0)}`;
 }
+
+const buttonChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+buttonChangeColor.addEventListener('click', () => {
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  spanColor.textContent = color;
+});
