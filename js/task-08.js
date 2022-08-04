039f14fb6d82b2e1fb64dc094d7f423d485f891f
strangedk <strangedik@gmail.com>
2022-08-04 22:41:10 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const {currentTarget} = event;
+  const { elements: { email, password }} = currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    alert('Please fill all the fields');
+  } else {
+    console.log(`email: ${email.value}, password: ${password.value}`);
+    currentTarget.reset();
+  }
+})
